76c0a065b4a6821af1fffbb62659e3507ff8c55a
James Daniels <james@marginleft.com>
2010-09-11 20:33:40 -0400

    Updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,18 @@
 class User < ActiveRecord::Base
 
   acts_as_authentic do |config|
-    config.with_options(:unless => :using_sso?) do |without_sso|
-      without_sso.merge_validates_confirmation_of_password_field_options
-      without_sso.merge_validates_length_of_password_field_options
-      without_sso.merge_validates_length_of_password_confirmation_field_options
-      without_sso.merge_validates_length_of_login_field_options
-      without_sso.merge_validates_format_of_login_field_options
-      without_sso.merge_validates_format_of_email_field_options
-      without_sso.merge_validates_length_of_email_field_options
-      without_sso.merge_validates_uniqueness_of_email_field_options
+    config.with_options(:allow_nil => lambda { |u| u.using_sso? }, :allow_blank => lambda { |u| u.using_sso? }) do |nil_with_sso|
+      nil_with_sso.merge_validates_confirmation_of_password_field_options
+      nil_with_sso.merge_validates_length_of_password_field_options
+      nil_with_sso.merge_validates_length_of_password_confirmation_field_options
+      nil_with_sso.merge_validates_length_of_login_field_options
+      nil_with_sso.merge_validates_format_of_login_field_options
+      nil_with_sso.merge_validates_format_of_email_field_options
+      nil_with_sso.merge_validates_length_of_email_field_options
+      nil_with_sso.merge_validates_uniqueness_of_email_field_options
+      nil_with_sso.merge_validates_uniqueness_of_login_field_options
+      nil_with_sso.merge_validates_uniqueness_of_email_field_options
     end
-    config.merge_validates_uniqueness_of_login_field_options :allow_nil => true
-    config.merge_validates_uniqueness_of_email_field_options :allow_nil => true
   end
 
   has_many :access_keys, :dependent => :destroy, :as => :owner
