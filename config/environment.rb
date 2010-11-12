f8bfbde73f6bbe6835a910a50cc42d32492f0fea
James Daniels <james@marginleft.com>
2010-11-12 15:22:15 -0600

    Fixed things up a little

--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,10 @@ RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  
-  config.gem 'authlogic'
-  config.gem 'lasso', :version => '0.2.0'
+ 
+ 	config.gem 'sqlite3-ruby', :version => '1.2.5', :lib => 'sqlite3'
+  config.gem 'authlogic',    :version => '~> 2.1.6'
+  config.gem 'lasso',        :version => '~> 0.3.0'
   
   config.time_zone = 'UTC'
   
